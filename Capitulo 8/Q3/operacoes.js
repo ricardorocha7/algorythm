a2575c8b69e26984f5dfb0ba87ba5c5b1f20d30b
ricardz <joaoricardorochalopes1@gmail.com>
2025-12-08 22:02:53 -0300

    Add files via upload

---
+++ Capitulo 8/Q3/operacoes.js
@@ -0,0 +1,21 @@
+function soma(a,b) {
+    return a+b;
+}
+
+function subtracao(a,b) {
+    return a-b;
+}
+
+function multiplicacao(a,b) {
+    return a*b;
+}
+
+function divisao(a,b) {
+    return a/b;
+}
+
+function exponenciacao(a,b) {
+    return a**b;
+}
+
+module.exports = {soma, subtracao, multiplicacao, divisao, exponenciacao};
