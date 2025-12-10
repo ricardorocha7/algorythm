4aebee13c315904518ffe8073d6fece7b469f44f
ricardz <joaoricardorochalopes1@gmail.com>
2025-12-10 17:31:22 -0300

    Delete beecrowd directory

--- beecrowd/1002.js
+++
@@ -1,8 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-var n = 3.14159;
-R = parseFloat(lines.shift());
-var A = n * (R*R);
-
-console.log(`A=${A.toFixed(4)}`);
