7d73a02a2bfd6becaabaea624fb7892fd4effc1f
ricardz <joaoricardorochalopes1@gmail.com>
2025-12-10 17:13:35 -0300

    Add aluno object with notes and log to console

---
+++ Capitulo 7/Q.1.js
@@ -0,0 +1,10 @@
+const aluno = {
+    nome: 'Vítor',
+    idade: 17,
+    curso: 'informática',
+    notas: [10, 9, 10, 9]
+}
+
+aluno.notas.push(7, 9)
+console.log(aluno.notas)
+module.exports = {};
